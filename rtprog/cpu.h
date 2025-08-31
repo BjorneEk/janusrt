6abb9cea4560ccbec39e0186df48c8b94ddf491d
Gustaf Franzen <gustaffranzen@icloud.com>
2025-08-31 18:11:03 +0200

    added interrupt enable/dissable

--- rtprog/cpu.h
+++ rtprog/cpu.h
@@ -36,4 +36,55 @@ static inline void halt(void)
 
 // in sync.c
 void sync_set_recover_handler(sync_recover_t recover);
+
+static inline void irq_disable(void)
+{
+	__asm__ volatile("msr daifset, #2" ::: "memory");
+}
+
+static inline void irq_enable(void)
+{
+	__asm__ volatile("msr daifclr, #2" ::: "memory");
+}
+
+static inline void fiq_disable(void)
+{
+	__asm__ volatile("msr daifset, #1" ::: "memory");
+}
+
+static inline void fiq_enable(void)
+{
+	__asm__ volatile("msr daifclr, #1" ::: "memory");
+}
+
+static inline void aborts_disable(void)
+{   // SError
+	__asm__ volatile("msr daifset, #4" ::: "memory");
+}
+
+static inline void aborts_enable(void)
+{
+	__asm__ volatile("msr daifclr, #4" ::: "memory");
+}
+
+static inline void debug_disable(void)
+{
+	__asm__ volatile("msr daifset, #8" ::: "memory");
+}
+
+static inline void debug_enable(void)
+{
+	__asm__ volatile("msr daifclr, #8" ::: "memory");
+}
+
+// Disable/enable all at once
+static inline void interrupts_disable_all(void)
+{
+	__asm__ volatile("msr daifset, #0xf" ::: "memory");
+}
+
+static inline void interrupts_enable_all(void)
+{
+	__asm__ volatile("msr daifclr, #0xf" ::: "memory");
+}
 #endif
