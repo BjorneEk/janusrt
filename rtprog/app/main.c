10366b787cbb85a00cc3312e219bc618fd4ab07a
Gustaf Franzen <gustaffranzen@icloud.com>
2025-09-01 15:29:20 +0200

    reverted to full test now that it works

--- rtprog/app/main.c
+++ rtprog/app/main.c
@@ -2,13 +2,11 @@
 
 int main(void *mem, u64 mem_size)
 {
-	/*
+
 	uart_puts("started with ");
 	uart_putu64(mem_size);
 	uart_puts(" bytes att address: ");
 	uart_puthex((uintptr_t)mem);
-	*/
 	uart_puts("\nterminating\n");
-
-	return (uintptr_t)mem + mem_size;
+	return 0;
 }
